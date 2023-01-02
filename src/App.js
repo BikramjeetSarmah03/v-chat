e5e17f302435283e9855bf0ba277f4c5c60f1384
Bikramjeet Sarmah <9854bikram@gmail.com>
2023-01-02 18:53:22 +0530

    proxy removed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Chats from "./pages/Chats";
 import { ChatState } from "./context/ChatProvider";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND;
 
 function App() {
   const { user } = ChatState();
